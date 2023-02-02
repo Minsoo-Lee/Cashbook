6870b2ac5590fdb6a8ee8e340abc5f8a97caca15
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-02-02 12:37:36 +0900

    [feat] upload cashbook project

---
+++ src/test/java/minsoo/cashbook/CashbookApplicationTests.java
@@ -0,0 +1,13 @@
+package minsoo.cashbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
