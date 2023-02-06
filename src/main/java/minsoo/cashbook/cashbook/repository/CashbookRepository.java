407d477a584962293e50a7e3c3858db9973429f4
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-02-06 18:37:05 +0900

    [feat] CashbookRepository interface

---
+++ src/main/java/minsoo/cashbook/cashbook/repository/CashbookRepository.java
@@ -0,0 +1,12 @@
+package minsoo.cashbook.cashbook.repository;
+
+import minsoo.cashbook.cashbook.domain.Cashbook;
+
+import java.util.List;
+
+public interface CashbookRepository {
+
+    void save(Cashbook cashbook);
+    List<Cashbook> findByMonth(String month);
+    List<Cashbook> findAll(String month);
+}
